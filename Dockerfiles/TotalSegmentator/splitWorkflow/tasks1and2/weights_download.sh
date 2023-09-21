05c2aafc42fd013c34db7db9305eaebc14e31095
vkt1414 <vthiriveedhi@mgh.harvard.edu>
2023-09-21 12:43:18 -0400

    Updated weights links and dockerfiles to start with sha256 base images

--- Dockerfiles/TotalSegmentator/splitWorkflow/tasks1and2/weights_download.sh
+++ Dockerfiles/TotalSegmentator/splitWorkflow/tasks1and2/weights_download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 weights_urls=(
-    "http://94.16.105.223/static/Task251_TotalSegmentator_part1_organs_1139subj.zip"
-    "http://94.16.105.223/static/Task252_TotalSegmentator_part2_vertebrae_1139subj.zip"
-    "http://94.16.105.223/static/Task253_TotalSegmentator_part3_cardiac_1139subj.zip"
-    "http://94.16.105.223/static/Task254_TotalSegmentator_part4_muscles_1139subj.zip"
-    "http://94.16.105.223/static/Task255_TotalSegmentator_part5_ribs_1139subj.zip"
-    "http://94.16.105.223/static/Task256_TotalSegmentator_3mm_1139subj.zip"
+  "https://github.com/wasserth/TotalSegmentator/releases/download/v1.5.6-weights/Task251_TotalSegmentator_part1_organs_1139subj.zip"
+  "https://github.com/wasserth/TotalSegmentator/releases/download/v1.5.6-weights/Task252_TotalSegmentator_part2_vertebrae_1139subj.zip"
+  "https://github.com/wasserth/TotalSegmentator/releases/download/v1.5.6-weights/Task253_TotalSegmentator_part3_cardiac_1139subj.zip"
+  "https://github.com/wasserth/TotalSegmentator/releases/download/v1.5.6-weights/Task254_TotalSegmentator_part4_muscles_1139subj.zip"
+  "https://github.com/wasserth/TotalSegmentator/releases/download/v1.5.6-weights/Task255_TotalSegmentator_part5_ribs_1139subj.zip"
+  "https://github.com/wasserth/TotalSegmentator/releases/download/v1.5.6-weights/Task256_TotalSegmentator_3mm_1139subj.zip"
 )
 
 weights_dir="${TOTALSEG_WEIGHTS_PATH}/nnUNet/3d_fullres/"
